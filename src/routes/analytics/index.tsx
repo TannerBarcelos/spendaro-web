86feb4d83fe2de36a38942d723454cc07623b19c
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-10-16 21:32:55 -0700

    Add analytics page component

---
+++ src/routes/analytics/index.tsx
@@ -0,0 +1,14 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/analytics/")({
+  component: () => <AnalyticsPage />,
+});
+
+function AnalyticsPage() {
+  return (
+    <div>
+      <h1 className="text-2xl">Analytics</h1>
+      <p>This is the analytics page.</p>
+    </div>
+  );
+}
