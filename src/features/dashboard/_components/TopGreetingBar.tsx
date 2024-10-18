39e4fe4a67883e9ebfaab5fcb1e65a7deafcc0ea
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-10-18 14:47:50 -0700

    Refactor TopGreetingBar component to use DropdownMenu for creating new budget and transaction

--- src/features/dashboard/_components/TopGreetingBar.tsx
+++ src/features/dashboard/_components/TopGreetingBar.tsx
@@ -1,17 +1,30 @@
 import { Button } from "@/components/ui/button";
-import { Link } from "@tanstack/react-router";
-import { HandCoins } from "lucide-react";
+import { HandCoins, Landmark, Plus } from "lucide-react";
 import { greetTimeOfDay as greet } from "@/lib/utils";
+import DropdownMenu from "@/components/DropdownMenu";
 
 function TopGreetingBar() {
   return (
-    <div className="flex flex-row items-center justify-between mb-4">
+    <div className="flex flex-row items-center mb-4 w-max">
       <h1 className="text-xl lg:text-2xl font-semibold">{greet()}</h1>
-      <Link to="/budget/new" className="flex items-center gap-2">
-        <Button className="rounded-lg text-sm">
-          <HandCoins /> create budget
+      <DropdownMenu
+        menuItems={[
+          {
+            name: "New Budget",
+            link: "/budget/new",
+            linkIcon: <HandCoins />,
+          },
+          {
+            name: "New Transaction",
+            link: "/transaction/new",
+            linkIcon: <Landmark />,
+          },
+        ]}
+      >
+        <Button className="text-sm w-10 h-10 rounded-full ml-4">
+          <Plus size={16} />
         </Button>
-      </Link>
+      </DropdownMenu>
     </div>
   );
 }
