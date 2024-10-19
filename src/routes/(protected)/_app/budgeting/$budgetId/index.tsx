291cb85d8b0414e69d8f394e28427e9ccc5446d0
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-10-19 13:15:02 -0700

    Refactor budgeting routes and imports for consistency

--- src/routes/(protected)/_app/budgeting/$budgetId/index.tsx
+++ src/routes/(protected)/_app/budgeting/$budgetId/index.tsx
@@ -1,6 +1,6 @@
-import { ViewBudget } from '@/features/budgets/budget/view-budget'
-import { createFileRoute } from '@tanstack/react-router'
+import { ViewBudget } from "@/features/budgeting/budget/view-budget";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/(protected)/_app/budgeting/$budgetId/')({
+export const Route = createFileRoute("/(protected)/_app/budgeting/$budgetId/")({
   component: ViewBudget,
-})
+});
