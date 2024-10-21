ff7b6ca94c96c92d7418ea0ca4e2d2b4975d9852
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-10-21 16:21:15 -0700

    Refactor Navigation component to extract unauthenticated navigation into separate component

--- src/features/auth/index.tsx
+++ src/features/auth/index.tsx
@@ -4,6 +4,21 @@ import TabsSelection from "./_components/tabs";
 import { Link } from "@tanstack/react-router";
 import { Zap } from "lucide-react";
 
+const UnAuthNav = () => {
+  return (
+    <nav className="p-4">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="flex flex-row p-3">
+          <Zap size={24} color="#006EFF" strokeWidth={2.5} className="mr-2" />
+          <Link to="/dashboard" className="text-xl font-semibold">
+            Spendaro
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
 export function AuthPage() {
   const [selectedTab, setSelectedTab] = useState("signin");
   const handleTabChange = (value: string) => {
@@ -11,16 +26,7 @@ export function AuthPage() {
   };
   return (
     <>
-      <nav className="p-4">
-        <div className="container mx-auto flex items-center justify-between">
-          <div className="flex flex-row p-3">
-            <Zap size={24} color="#006EFF" strokeWidth={2.5} className="mr-2" />
-            <Link to="/dashboard" className="text-xl font-semibold">
-              Spendaro
-            </Link>
-          </div>
-        </div>
-      </nav>
+      <UnAuthNav />
       <div className="flex flex-col items-center justify-center p-4 mt-20">
         <div className="w-full max-w-lg">
           <div className="bg-white rounded-xl shadow-2xl p-8 space-y-8">
