c13a916049b7070555b585160b18b06826cea57a
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-10-29 14:01:58 -0700

    Refactor UserProfilePage to use generateUploadDropzone for profile image uploads

--- src/features/profile/index.tsx
+++ src/features/profile/index.tsx
@@ -1,14 +1,21 @@
-import { generateUploadButton } from "@uploadthing/react";
+import { generateUploadDropzone } from "@uploadthing/react";
 import { twMerge } from "tailwind-merge";
 
 export function UserProfilePage() {
-  const UploadButton = generateUploadButton({
+  const UploadImageDropzone = generateUploadDropzone({
     url: "http://localhost:8010/api/uploadthing",
   });
   return (
     <div>
       <h1 className="text-xl lg:text-2xl font-semibold">User Profile</h1>
-      <UploadButton endpoint="profileImageUploader" config={{ cn: twMerge }} />
+      <div className="my-10">
+        <h2 className="text-lg font-semibold">Profile Image</h2>
+        <UploadImageDropzone
+          endpoint="profileImageUploader"
+          config={{ cn: twMerge }}
+          className="w-[400px]"
+        />
+      </div>
     </div>
   );
 }
