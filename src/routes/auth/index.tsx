5ff396aa6b3f5ab6f45be5b440daddb6dd97a73d
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-11-06 14:02:39 -0800

    Update font family in CSS, enhance search params validation in auth routes, and refine Tailwind CSS configuration

--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -3,8 +3,9 @@ import { AuthPage } from "@/features/auth";
 import { z } from "zod";
 import { isAuthenticated } from "@/stores/auth-store";
 
+// Define the search params schema - can access the search params using the useSearch hook attached to the Route component i.e. AuthRoute.useSearch()
 const searchParamsSchema = z.object({
-  redirect_url: z.string().optional(),
+  redirect_url: z.string().optional(), // Optional because it's not always present - e.g. when user directly visits /auth/signin or they logout and are redirected to /auth/signin which doesn't have a redirect_url as a full logout means signing in again takes us to the dashboard
 });
 
 export const Route = createFileRoute("/auth/")({
@@ -13,6 +14,7 @@ export const Route = createFileRoute("/auth/")({
       throw redirect({ to: "/dashboard" });
     }
   },
+  // Validate the search params
   validateSearch: (search: z.infer<typeof searchParamsSchema>) => {
     return searchParamsSchema.parse(search);
   },
