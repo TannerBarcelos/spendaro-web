01dc14fd9dcfd1b83c9de5fd6e963f2f4898b968
Tanner Barcelos <41309200+TannerBarcelos@users.noreply.github.com>
2024-11-08 23:28:59 -0800

    Update axios.ts

---
+++ src/api/axios.ts
@@ -0,0 +1,79 @@
+import { authStore } from "@/stores/auth-store";
+import axios, { AxiosError } from "axios";
+
+export type CommonApiErrorResponse = AxiosError<{
+  error: string;
+  message: string;
+  details: {
+    issues: string[];
+    method: string;
+    stack?: string;
+    url: string;
+  };
+}>;
+
+const commonHeaders = {
+  "Content-Type": "application/json",
+  Accept: "application/json",
+};
+
+const commonAxionConfig = {
+  baseURL: "/api/v1",
+  timeout: 5_000,
+};
+
+const axiosInstance = axios.create({
+  ...commonAxionConfig,
+  headers: {
+    ...commonHeaders,
+  },
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    // Check if the error is 403 Forbidden and it's not the auth request itself, and also prevent retry loops
+    if (
+      error.response?.status === axios.HttpStatusCode.Forbidden &&
+      !originalRequest.url?.includes("auth") &&
+      !originalRequest.sent
+    ) {
+      try {
+        // Mark the request to prevent retry loops
+        originalRequest.sent = true;
+
+        // Attempt to refresh the token
+        const response = await axiosInstance.post(
+          "/auth/refresh",
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${authStore.getState().refreshToken}`, // will override the default Authorization header
+            },
+          }
+        );
+
+        const setAccessTokens = authStore.getState().setAccessTokens
+        setAccessTokens(response.data.accessToken, response.data.refreshToken)
+
+        // If refresh is successful, retry the original request with the new token
+        if (response.status === axios.HttpStatusCode.Ok) {
+          axiosInstance.defaults.headers['Authorization'] = `Bearer ${response.data.accessToken}`;
+          return axiosInstance(originalRequest);
+        }
+      } catch (refreshError) {
+        const logout = authStore.getState().clear;
+        localStorage.clear();
+        logout();
+        window.location.href = "/signin";
+      }
+    }
+
+    // Reject the original error if refresh was not possible
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
