63ad81f7a84f748d4b950cd4806830e868d5e17c
Tanner Barcelos <41309200+TannerBarcelos@users.noreply.github.com>
2024-11-08 23:31:51 -0800

    Update index.ts

---
+++ src/api/transactions/index.ts
@@ -0,0 +1,21 @@
+import axiosInstance from '@/lib/axios';
+
+export const getTransactions = async () => {
+  const response = await axiosInstance.get('/transactions');
+  return response.data;
+};
+
+export const createTransaction = async (transactionData: any) => {
+  const response = await axiosInstance.post('/transactions', transactionData);
+  return response.data;
+};
+
+export const updateTransaction = async (id: string, transactionData: any) => {
+  const response = await axiosInstance.put(`/transactions/${id}`, transactionData);
+  return response.data;
+};
+
+export const deleteTransaction = async (id: string) => {
+  const response = await axiosInstance.delete(`/transactions/${id}`);
+  return response.data;
+};
