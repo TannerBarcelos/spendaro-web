452e589b9f853c8dcd5c75978a0f5f4ceee51997
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-11-11 17:35:55 -0800

    Add recharts for data visualization in ReportingWidget and update styles

--- src/features/dashboard/panels/reporting/index.tsx
+++ src/features/dashboard/panels/reporting/index.tsx
@@ -1,5 +1,12 @@
 import Info from "@/components/Info";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+} from "@/components/ui/card";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,12 +15,22 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Link } from "@tanstack/react-router";
 import { ChartArea, EllipsisVertical, EyeIcon, Plus } from "lucide-react";
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
 
 export function ReportingWidget() {
   return (
     <Card className="col-span-3 row-span-2">
       <WidgetTitle />
-      <CardContent></CardContent>
+      <CardContent>
+        <Chart />
+      </CardContent>
     </Card>
   );
 }
@@ -55,5 +72,43 @@ const WidgetTitle = () => {
         </DropdownMenu>
       </CardTitle>
     </CardHeader>
+  );
+};
+
+const Chart = () => {
+  const chartData = [
+    { month: "January", desktop: 186 },
+    { month: "February", desktop: 305 },
+    { month: "March", desktop: 237 },
+    { month: "April", desktop: 73 },
+    { month: "May", desktop: 209 },
+    { month: "June", desktop: 214 },
+  ];
+
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "hsl(var(--chart-1))",
+    },
+  } satisfies ChartConfig;
+
+  return (
+    <ChartContainer config={chartConfig}>
+      <BarChart accessibilityLayer data={chartData}>
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey="month"
+          tickLine={false}
+          tickMargin={5}
+          axisLine={false}
+          tickFormatter={(value) => value.slice(0, 3)}
+        />
+        <ChartTooltip
+          cursor={false}
+          content={<ChartTooltipContent hideLabel />}
+        />
+        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={8} />
+      </BarChart>
+    </ChartContainer>
   );
 };
