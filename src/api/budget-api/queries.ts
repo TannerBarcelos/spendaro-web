31178982a26e80f8b0cc669f060ae0c0f64c7b75
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-11-16 18:21:01 -0800

    Remove unused comments and clean up budget API query code

--- src/api/budget-api/queries.ts
+++ src/api/budget-api/queries.ts
@@ -2,17 +2,6 @@ import axiosInstance from "@/api/axios";
 import { BudgetApiResponse, BudgetsApiResponse } from "./types";
 import { useQuery } from "@tanstack/react-query";
 
-
-// Note that this "query hook" does not use useQuery from react-query. This is because
-// we are using the query via our context in the Tanstack Router Loader. This is a
-// different way to use the query, but it is still a query hook. _see /src/routes/(protected)/_app/budgeting/$budgetId/index.tsx_
-
-// export const Route = createFileRoute("/(protected)/_app/budgeting/$budgetId/")({
-//   component: ViewBudget,
-//   loader: ({ context: { queryClient }, params: { budgetId } }) => {
-//     return queryClient.ensureQueryData(getBudgetOptions(budgetId));
-//   },
-// });
 export const getBudget = (budget_id: string) => {
   return {
     queryKey: ["budget", budget_id],
