8638e4befac650929f1572f3b1d65337e24bf0b9
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-11-16 16:56:57 -0800

    Clerk setup

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,14 @@ import { RouterProvider } from "@tanstack/react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/api/query-client.ts";
 import { router as routerInstance } from "./router.ts";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+// Clerk
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 // we must export the router instance in main.tsx so that Tanstack Router can register properly (we could have also defined the router instance in this file, but I am separating concerns for better organization)
 export const router = routerInstance;
@@ -17,11 +25,13 @@ if (!rootElement.innerHTML) {
   const root = createRoot(rootElement);
   root.render(
     <StrictMode>
-      <ThemeProvider>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-        </QueryClientProvider>
-      </ThemeProvider>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/signin">
+        <ThemeProvider>
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+          </QueryClientProvider>
+        </ThemeProvider>
+      </ClerkProvider>
     </StrictMode>
   );
 }
