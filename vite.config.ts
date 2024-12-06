dc2d450046c7dd786fb138448681bcd4f8908adf
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-12-06 12:31:29 -0800

    Update Vite configuration to load environment variables for API proxy target

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,27 @@
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
-  },
-  server: {
-    proxy: {
-      "/api/v1": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
+    plugins: [TanStackRouterVite(), react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-
-  }
-})
+    server: {
+      proxy: {
+        "/api/v1": {
+          target: env.VITE_API_URL || "http://localhost:8080",
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
